a511dc9b1c75346b89dc1db4a5d5b6e8fcdb0efe
Rajesh Elumalai <rajesh.elumalai@gmail.com>
2025-11-20 19:20:50 +0000

    rest

--- rekapp/srv/service.cds
+++ rekapp/srv/service.cds
@@ -1,3 +1,7 @@
+@protocol: 'rest'
+
 service product {
     function printhelloworld (input:String) returns String;
+
+    function joinString(str1: String , str2: String) returns String;
 }
--- rekapp/srv/service.js
+++ rekapp/srv/service.js
@@ -5,4 +5,11 @@ module.exports = cds.service.impl(async function(srv) {
         console.log(req.data.input)
         return `${req.data.input} World`
     })
+
+    srv.on('joinString', req =>{
+        console.log(req.data.str1);
+        console.log(req.data.str2);
+        let output = req.data.str1 + ' ' + req.data.str2;
+        return `${output}`
+    })
 })
